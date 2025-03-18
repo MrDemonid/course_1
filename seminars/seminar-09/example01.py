e1b7ac42b951d87bd1d1fdee36bfe2ff1d5b76ef
MrDemonid <MrDemonid@yandex.ru>
2025-03-18 23:27:48 +0300

    begin seminar 9

--- seminars/seminar-09/example01.py
+++ seminars/seminar-09/example01.py
@@ -8,3 +8,23 @@
 угадать загаданное число за указанное число попыток.
 """
 
+def main_decorator():
+    number = int(input("Enter number from range 1-100: "))
+    count = int(input("Enter changes from range 1-10: "))
+    def bulls_and_cows():
+        n = -number
+        for _ in range(count):
+            n = int(input("-- my number (1-100)?: "))
+            if n == number:
+                print("You win!")
+                return
+            if n > number:
+                print("  -- my number is less!")
+            else:
+                print("  -- my number is great!")
+        print(f"You loss! number is {number}")
+    return bulls_and_cows
+
+
+f = main_decorator()
+f()
--- seminars/seminar-09/example02.py
+++ seminars/seminar-09/example02.py
@@ -6,3 +6,36 @@
 Он должен проверять входят ли переданные в функцию-угадайку числа в диапазоны [1, 100] и [1, 10].
 Если не входят, вызывать функцию со случайными числами из диапазонов.
 """
+import random
+from typing import Callable
+
+
+def run_func(func: Callable):
+    def wrapper(num: int, count: int):
+        if num < 1 or num > 100:
+            num = random.randint(1, 100)
+        if count < 1 or count > 10:
+            count = random.randint(1, 10)
+        func(num, count)
+
+    return wrapper
+
+
+@run_func
+def bulls_and_cows(num: int, count: int):
+    n = -num
+    for _ in range(count):
+        n = int(input("-- my number (1-100)?: "))
+        if n == num:
+            print("You win!")
+            return
+        if n > num:
+            print("  -- my number is less!")
+        else:
+            print("  -- my number is great!")
+    print(f"You loss! number is {num}")
+
+
+# run_func(bulls_and_cows)(1000, 0)
+bulls_and_cows(1000, 0)
+
