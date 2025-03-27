dfebe5da86e9b2caac94eeff4f4097ade7f1ee40
MrDemonid <MrDemonid@yandex.ru>
2025-03-27 22:01:48 +0300

    proceed homework - make Tic-Tac-Toe game

--- homeworks/homework-10/work2.py
+++ homeworks/homework-10/work2.py
@@ -29,3 +29,138 @@
 Реализуйте такую программу и создайте двух людей, живущих в одном доме.
 Проверьте работу программы несколько раз
 """
+from random import randint
+
+
+class House:
+    def __init__(self):
+        self.food = 50
+        self.many = 0
+
+    def get_food(self):
+        return self.food
+
+    def get_many(self):
+        return self.many
+
+    def inc_food(self):
+        self.food += 1
+
+    def give_food(self):
+        if self.food > 0:
+            self.food -= 1
+            return True
+        return False
+
+    def inc_many(self):
+        self.many += 1
+
+    def give_many(self):
+        if self.many > 0:
+            self.many -= 1
+            return True
+        return False
+
+
+class Person:
+    def __init__(self, name, house):
+        self.name = name
+        self.hungry = 50
+        self.house = house
+        self._eats = 0
+        self._plays = 0
+        self._works = 0
+        self._go_true = 0
+        self._go_fail = 0
+
+
+    def eat(self):
+        if self.house.give_food():
+            self._eats += 1
+            self.hungry += 1
+            return True
+        return False
+
+    def work(self):
+        self.hungry -= 1
+        self.house.many += 1
+        self._works += 1
+
+    def play(self):
+        self.hungry -= 1
+        self._plays += 1
+
+    def go_store(self):
+        if self.house.give_many():
+            self.house.inc_food()
+            self._go_true += 1
+            return True
+        self._go_fail += 1
+        return False
+
+    def check(self):
+        if self.hungry >= 0:
+            return True
+        return False
+
+    def get_total_actions(self):
+        return self._eats + self._works + self._plays + self._go_true + self._go_fail
+
+    def live_day(self):
+        step = randint(1, 6)
+        if self.hungry < 20:
+            if not self.eat():
+                print("Внимание! Нечего есть!")
+        elif self.house.get_food() < 10:
+            if not self.go_store():
+                print("Внимание! Нет денег на покупку еды!")
+        elif self.house.get_many() < 50:
+            self.work()
+        elif step == 1:
+            self.work()
+        elif step == 2:
+            self.eat()
+        else:
+            self.play()
+        return self.check()
+
+    def __str__(self):
+        return (f"{self.name}: hungry = {self.hungry}, "
+                f"food = {self.house.get_food()}, "
+                f"many = {self.house.get_many()}, "
+                f"eats = {self._eats}, works = {self._works}, plays = {self._plays}, "
+                f"go to store = {self._go_true}, goto fail = {self._go_fail}, "
+                f"total actions = {self.get_total_actions()}"
+                )
+
+
+def simulate(p1: Person, p2: Person, days):
+    while days > 0:
+        if not p1.live_day() or not p2.live_day():
+            return False
+        days -= 1
+    return True
+
+def multi_test(count):
+    while count > 0:
+        h = House()
+        p1 = Person("Ivan", h)
+        p2 = Person("Elena", h)
+        res = simulate(p1, p2, 365)
+        print(("Выжили" if res else "Неудача"))
+        print(p1)
+        print(p2)
+        count -= 1
+
+multi_test(3)
+
+# Выжили
+# Ivan: hungry = 19, food = 10, many = 50, eats = 118, works = 121, plays = 28, go to store = 98, goto fail = 0, total actions = 365
+# Elena: hungry = 19, food = 10, many = 50, eats = 118, works = 125, plays = 24, go to store = 98, goto fail = 0, total actions = 365
+# Выжили
+# Ivan: hungry = 20, food = 9, many = 49, eats = 119, works = 123, plays = 26, go to store = 97, goto fail = 0, total actions = 365
+# Elena: hungry = 20, food = 9, many = 49, eats = 118, works = 122, plays = 26, go to store = 99, goto fail = 0, total actions = 365
+# Выжили
+# Ivan: hungry = 19, food = 10, many = 50, eats = 118, works = 123, plays = 26, go to store = 98, goto fail = 0, total actions = 365
+# Elena: hungry = 19, food = 10, many = 50, eats = 118, works = 123, plays = 26, go to store = 98, goto fail = 0, total actions = 365
+
--- homeworks/homework-10/work3.py
+++ homeworks/homework-10/work3.py
@@ -42,3 +42,98 @@ class Game:
           продолжать играть. После каждой игры выводится текущий счёт игроков.
 """
 
+
+class Cell:
+    def __init__(self, number):
+        self.number = number
+        self.symbol = ' '
+
+    def is_empty(self):
+        return self.symbol == ' '
+
+    def get_symbol(self):
+        return self.symbol
+
+    def set_symbol(self, symbol):
+        self.symbol = symbol
+
+    def get_number(self):
+        return self.number
+
+    def __str__(self):
+        return f"{self.symbol}"
+
+
+class Board:
+
+    def __init__(self, size):
+        self._size = size
+        self.cells = [Cell(i) for i in range(1, size * size + 1)]
+        self.__make_tables(size)  # готовим таблицу для определения состояния игры
+
+    def __make_tables(self, size):
+        """ Создает таблицу с номерами ячеек, для быстрой проверки на чей-то выигрыш"""
+        self._table = []
+        # горизонталь и вертикаль
+        for y in range(size):
+            hor = []
+            ver = []
+            for x in range(size):
+                hor.append(y * size + x)
+                ver.append(y + x * size)
+            self._table.append(hor)
+            self._table.append(ver)
+        # диагонали
+        d1 = []
+        d2 = []
+        for i in range(size):
+            d1.append(i * size + i)
+            d2.append((size - i - 1) * size + i)
+        self._table.append(d1)
+        self._table.append(d2)
+
+    def reset_board(self):
+        """ Очистка игрового поля """
+        for cell in self.cells:
+            cell.set_symbol(' ')
+
+    def change_cell(self, cell_number, symbol):
+        """ Изменяет состояние ячейки, если она не занята """
+        if (0 < cell_number <= len(self.cells)) and (symbol == 'x' or symbol == 'o'):
+            cell = self.cells[cell_number - 1]
+            if cell.is_empty():
+                cell.set_symbol(symbol)
+                return True
+        return False
+
+    def check_game(self):
+        """ Проверка, не выиграл ли один из игроков"""
+        for line in self._table:
+            pos = line[0]
+            ch = self.cells[pos].get_symbol()
+            cnt = 0
+            if not self.cells[pos].is_empty():
+                for i in line:
+                    if self.cells[i].get_symbol() == ch:
+                        cnt += 1
+            if cnt == len(line):
+                return True
+        return False
+
+    def show(self):
+        """ Вывод доски в консоль """
+        for y in range(self._size):
+            print('+---+---+---+')
+            n = []
+            for x in range(self._size):
+                n.append(self.cells[y*self._size+x].get_symbol())
+            s = " | ".join(n)
+            print(f"| {s} |")
+        print('+---+---+---+')
+
+
+b = Board(3)
+b.change_cell(1, 'x')
+b.change_cell(5, 'x')
+b.change_cell(3, 'o')
+b.show()
