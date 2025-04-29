bdf1e0bec9d248da6c3024df2b6d83bedcb106d7
MrDemonid <MrDemonid@yandex.ru>
2025-04-29 11:23:50 +0300

    finish homework 14

--- homeworks/homework-14/work2.py
+++ homeworks/homework-14/work2.py
@@ -9,3 +9,74 @@
 При попытке удалить книгу, которая не существует, должно выбрасываться исключение BookNotFoundError.
 Для тестирования используйте unitest.
 """
+
+
+class BookNotFoundError(Exception):
+    def __init__(self):
+        super().__init__("Книга не найдена!")
+
+
+class Library:
+    def __init__(self, titles: list):
+        self._titles = list()
+        for title in titles:
+            self._titles.append(title)
+
+    def add_book(self, title):
+        if isinstance(title, str):
+            if title is None or len(title.strip()) == 0:
+                raise ValueError("Название не может быть пустой строкой!")
+            if title in self._titles:
+                raise ValueError("Книга уже присутствует в библиотеке!")
+            self._titles.append(title)
+        else:
+            raise TypeError("Название должно быть строкой!")
+
+    def remove_book(self, title):
+        if isinstance(title, str):
+            if not title in self._titles:
+                raise BookNotFoundError()
+            self._titles.remove(title)
+        else:
+            raise TypeError("Название должно быть строкой!")
+
+    def list_books(self):
+        res = list()
+        for t in self._titles:
+            res.append(t)
+        return res
+
+
+import unittest
+
+class TestLibrary(unittest.TestCase):
+
+    def setUp(self):
+        self.lib = Library(['Туманность Андромеды', 'Маленькие дикари'])
+
+    def test_init(self):
+        self.assertEqual(self.lib.list_books(), ['Туманность Андромеды', 'Маленькие дикари'])
+
+    def test_add_normal(self):
+        self.lib.add_book('Посёлок')
+        self.assertIn('Посёлок', self.lib.list_books())
+
+    def test_add_value(self):
+        self.assertRaises(ValueError, self.lib.add_book, '')
+
+    def test_add_type(self):
+        self.assertRaises(TypeError, self.lib.add_book, 1974)
+
+    def test_remove_normal(self):
+        self.lib.remove_book('Туманность Андромеды')
+        self.assertNotIn('Туманность Андромеды', self.lib.list_books())
+
+    def test_remove_except(self):
+        self.assertRaises(BookNotFoundError, self.lib.remove_book, 'Посёлок')
+
+    def test_remove_type(self):
+        self.assertRaises(TypeError, self.lib.remove_book, 1974)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- homeworks/homework-14/work3.py
+++ homeworks/homework-14/work3.py
@@ -8,3 +8,85 @@
 
 Напишите 3 теста с помощью doctest.
 """
+import doctest
+
+
+class Rectangle:
+    """ Класс прямоугольника.
+    >>> Rectangle(12, 0)
+    Traceback (most recent call last):
+    ...
+    ValueError: Некорректная высота для прямоугольника!
+
+    >>> Rectangle('string', 10)
+    Traceback (most recent call last):
+    ...
+    ValueError: Некорректная ширина для прямоугольника!
+
+    >>> r = Rectangle(4, 2)
+    >>> r.get_area()
+    8
+
+    >>> r = Rectangle(4, 2)
+    >>> r.get_perimeter()
+    12
+
+    >>> r = Rectangle(4, 3)
+    >>> r.set_dimension(-12, 10)
+    Traceback (most recent call last):
+    ...
+    ValueError: Некорректная ширина для прямоугольника!
+
+    >>> r = Rectangle(4, 3)
+    >>> r.set_dimension(12, 0)
+    Traceback (most recent call last):
+    ...
+    ValueError: Некорректная высота для прямоугольника!
+    """
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self._width
+
+    @width.setter
+    def width(self, new_width):
+        if isinstance(new_width, (int, float)) and new_width > 0:
+            self._width = new_width
+        else:
+            raise ValueError("Некорректная ширина для прямоугольника!")
+
+    @property
+    def height(self):
+        return self._height
+
+    @height.setter
+    def height(self, new_height):
+        if isinstance(new_height, (int, float)) and new_height > 0:
+            self._height = new_height
+        else:
+            raise ValueError("Некорректная высота для прямоугольника!")
+
+    def set_dimension(self, width, height):
+        self.width = width
+        self.height = height
+
+    def get_area(self):
+        return self.width * self.height
+
+    def get_perimeter(self):
+        return self.width * 2 + self.height * 2
+
+    def __repr__(self):
+        return f"Rectangle({self.width}, {self.height})"
+
+    def __str__(self):
+        return f"Прямоугольник. Ширина = {self.width}. Высота = {self.height}."
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(verbose=True)
+
